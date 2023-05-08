8df87b2bf5ef13efa5f7d87cf6d1935d662fc512
Marouane el Jaghnouni <2179336@talnet.nl>
2023-05-08 10:46:59 +0200

    commit1

---
+++ opdracht.php
@@ -0,0 +1,23 @@
+<?php
+$waarde1 = 12;
+$waarde2 = 10;
+$resultaat = $waarde1 + $waarde1;
+echo $resultaat;
+
+$waarde1 = 12;
+$waarde2 = 10;
+$resultaat = $waarde1 * $waarde1;
+echo $resultaat;
+
+$naam = "Marouane";
+
+echo "Welkom " . $naam;
+
+//  deze code bevat veel fouten
+// haal alle fouten eruit!u
+
+$mijnNaam = "Carl";
+$jouwNaam= "Camel";
+echo $mijnNaam + " zit bij " + $jouwNaam + " in de klas!"
+
+?>
